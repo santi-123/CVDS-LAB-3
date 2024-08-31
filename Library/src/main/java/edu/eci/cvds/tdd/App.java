b331de2c379d3a5c973a730b2bf9d9b6eeebcbcc
David Velasquez <dafeveco2004@gmail.com>
2024-08-31 12:44:58 -0500

    error no compila

--- Library/src/main/java/edu/eci/cvds/tdd/App.java
+++ Library/src/main/java/edu/eci/cvds/tdd/App.java
@@ -1,13 +1,15 @@
-package edu.eci.cvds.tdd;
+package edu.eci.cvds.tdd.library;
+
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
     }
 }
